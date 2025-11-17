4bcf1d1a2c8f86d0244b9497a04342c2143b2bdf
itramb <60043400+itramb@users.noreply.github.com>
2025-11-17 18:15:47 +0300

    Delete docs directory

--- docs/scripts/custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('Custom element loaded!');
-    // Initialize Fancybox
-    Fancybox.bind('[data-fancybox]', {
-        // Custom options
-    });
-})
